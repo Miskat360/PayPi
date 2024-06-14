dd9a0cc540b9e7d5bd7366cc080e8ec67aaa1a73
user.name "Miskat Hasan <user.email "miskathasan979@gmail.com>
2024-06-14 18:50:38 +0600

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["index.html"],
   theme: {
+    screens:{
+      'xs': '350px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    },
     extend: {
       colors: {
         blue1: "#598DC7",
